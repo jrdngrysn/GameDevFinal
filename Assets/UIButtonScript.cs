0bb1eb892ecdeefde0ee792c8db7c4019dfd68b1
Jordan Grayson <jtg400@nyu.edu>
2019-05-01 15:27:59 -0400

    Continued work on moving UI. The statbar now moves, but does not highlight yet.

--- Assets/UIButtonScript.cs
+++ Assets/UIButtonScript.cs
@@ -4,54 +4,87 @@ using UnityEngine;
 
 public class UIButtonScript : MonoBehaviour
 {
-    public GameObject movableUI;
-    Vector3 startingPos;
-    public GameObject lerpTarget;
-    Vector3 endingPos;
-    public float lerpTime = 1f;
-    bool isLerping = false;
-    string textPosition = "lower";
 
+     Animator animator;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        startingPos = movableUI.transform.localPosition;
-        endingPos = lerpTarget.transform.localPosition;
+        animator = GetComponent<Animator>();
+        animator.SetBool("PressedBool", false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-  
-        if (isLerping)
-        {
-            if (textPosition == "upper")
-            {
-                movableUI.transform.localPosition = Vector3.Lerp(startingPos, endingPos, lerpTime);
-                //Debug.Log("lerp up");
+   
 
-            } else if (textPosition == "lower")
-            {
-                movableUI.transform.localPosition = Vector3.Lerp(endingPos, startingPos, lerpTime);
-                //Debug.Log("lerp down");
-            }
+    public void WhenCLicked()
+    {
+        if (animator.GetBool("PressedBool") == false)
+        {
+            animator.SetBool("PressedBool", true);
         }
-    }
-
-    public void WhenClicked()
-    {
-        if (textPosition == "lower")
+        else
         {
-            isLerping = true;
-            textPosition = "upper";
-
-        } else if (textPosition == "upper")
-        {
-            isLerping = true;
-            textPosition = "lower";
+            animator.SetBool("PressedBool", false);
         }
 
     }
+
+
+
+
+
+
+
+
+
+
+    //    public GameObject movableUI;
+    //    Vector3 startingPos;
+    //    public GameObject lerpTarget;
+    //    Vector3 endingPos;
+    //    public float lerpTime = .1f;
+    //    bool isLerping = false;
+    //    string textPosition = "lower";
+
+
+
+    //    // Start is called before the first frame update
+    //    void Start()
+    //    {
+    //        startingPos = movableUI.transform.localPosition;
+    //        endingPos = lerpTarget.transform.localPosition;
+    //    }
+
+    //    // Update is called once per frame
+    //    void Update()
+    //    {
+
+    //        if (isLerping)
+    //        {
+    //            if (transform.localPosition.y <= endingPos.y)
+    //            {
+    //                movableUI.transform.localPosition = Vector3.Lerp(startingPos, endingPos, Time.time / lerpTime);
+    //                //Debug.Log("lerp up");
+
+    //            } else if (transform.localPosition.y >= startingPos.y)
+    //            {
+    //                movableUI.transform.localPosition = Vector3.Lerp(endingPos, startingPos, Time.time / lerpTime);
+    //                //Debug.Log("lerp down");
+    //            }
+    //        }
+    //    }
+
+    //    public void WhenClicked()
+    //    {
+    //        if (textPosition == "lower")
+    //        {
+    //            isLerping = true;
+    //            textPosition = "upper";
+
+    //        } else if (textPosition == "upper")
+    //        {
+    //            isLerping = true;
+    //            textPosition = "lower";
+    //        }
+
+    //    }
 }
